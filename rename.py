5963dee67f68232b2189d545d0df8363a5033f0d
Ryan <ryan.johnson.j@gmail.com>
2017-10-22 01:09:56 -0400

    Initial Upload
    Created

---
+++ rename.py
@@ -0,0 +1,16 @@
+#!python3
+import sys, os, time
+
+print ("Current Directory is %s \n" %os.getcwd())
+cont = input("Rename Files in current directory (Y)es, (N)o\n")
+path =  os.getcwd()
+if cont in ["Y", "y", "Yes", "No"]:
+    for orig_name in os.listdir():
+        if not orig_name.startswith("S"):
+            addS = "S" + orig_name
+            newname = addS.replace(".", "E", 1)
+            print(orig_name, "  >>  ", newname)
+            os.rename(orig_name, newname)
+            time.sleep(1)
+else:
+    quit()
